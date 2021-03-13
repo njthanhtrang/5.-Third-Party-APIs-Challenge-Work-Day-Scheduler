8af9696483f0dcefe824a4b23650631e39108b60
njthanhtrang <njthanhtrang@users.noreply.github.com>
2021-03-13 11:17:35 -0800

    refactored loadEvents

--- Develop/script.js
+++ Develop/script.js
@@ -12,67 +12,77 @@ var currentHour = parseInt(moment().format("H"));
 
 // refresh page, saved events persist
 var loadEvents = function (timeSlots) {
-    // console.log(localStorage);
+  // events = JSON.parse(localStorage.getItem(time, text) || "[]");
+  // for (var i = 0; i < timeSlots.length; i++) {
+  //     console.log(timeSlots[i]);
+  //     let time = localStorage.getItem(parseInt(timeSlots[i].time));
+  //     if (time) {
+  //         timeSlots[i].text.val(time);
+  //     }
+  // }
 
-    for (var i = 0; i < timeSlots.length; i++) {
-        // console.log(timeSlots[i]);
-        let time = localStorage.getItem(parseInt(timeSlots[i].time));
-        if (time) {
-            timeSlots[i].text.val(time);
-        }
+  // each element is an instance of the time, text obj
+  timeSlots.forEach((element) => {
+    console.log(element);
+    let text = localStorage.getItem(parseInt(element.time));
+    console.log(text);
+    if (text) {
+      element.text.val(text);
     }
+  });
 };
 
 var fetchEvents = function () {
-    var tempArr = [];
-    $("textarea").each(function (index, elem) {
-        tempArr.push({
-            time: $(elem)
-                .attr("id"),
-            text: $(elem)
-        });
+  var tempArr = [];
+  // array iterator method
+  $("textarea").each(function (index, elem) {
+    tempArr.push({
+      time: $(elem).attr("id"),
+      text: $(elem),
     });
-    loadEvents(tempArr);
-}
+  });
+  loadEvents(tempArr);
+};
 
 // time blocks COLOR CODED to indicate past, present, or future
 $("textarea").each(function () {
-    var $this = $(this);
-    var id = parseInt($this.attr("id"));
+  var $this = $(this);
+  var id = parseInt($this.attr("id"));
 
-    if (id < currentHour) {
-        $(this).addClass("past");
-    }
-    if (id > currentHour) {
-        $(this).addClass("future");
-    }
-    if (id === currentHour) {
-        $(this).addClass("present");
-    }
+  if (id < currentHour) {
+    $(this).addClass("past");
+  }
+  if (id > currentHour) {
+    $(this).addClass("future");
+  }
+  if (id === currentHour) {
+    $(this).addClass("present");
+  }
 });
 
 // click save button to add value to events
 $("button.saveBtn").click(function (event, loadEvents) {
-    event.preventDefault();
+  event.preventDefault();
 
-    // $(this) current button being clicked
-    var $element = $(this).siblings("textarea");
-    // console.log($element);
-    // get time via id attribute
-    var time = $element.attr("id");
-    // console.log(time);
-    // get text content via $.val()
-    var text = $element.val().trim();
-    // console.log(text);
+  // $(this) current button being clicked
+  var $element = $(this).siblings("textarea");
+  // console.log($element);
+  // get time via id attribute
+  var time = $element.attr("id");
+  console.log(time);
+  // get text content via $.val()
+  var text = $element.val().trim();
+  console.log(text);
 
-    // save events to localStorage
-    if (time && text !== "") {
-        localStorage.setItem(time, text);
-    };
+  // save events to localStorage
+  if (time && text !== "") {
+    console.log(time, text);
+    localStorage.setItem(time, text);
+  }
 });
 
 $(".saveBtn").hover(function () {
-    $(this).addClass("saveBtn:hover");
+  $(this).addClass("saveBtn:hover");
 });
 
 fetchEvents();
